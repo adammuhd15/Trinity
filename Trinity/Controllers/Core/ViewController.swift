9129a5091fa015e1fe00aacb1f1822ab27b07649
adammuhd15 <adam_muhd96@hotmail.co.uk>
2024-06-19 11:19:13 +0800

    initial commit

---
+++ Trinity/Controllers/Core/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trinity
+//
+//  Created by Adam Muhammad on 19/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
